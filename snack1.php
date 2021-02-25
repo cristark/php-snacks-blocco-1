d38244e737311fd439a9fec491320add28f51de7
Cristiano <cristiano.tarconi@gmail.com>
2021-02-25 16:07:54 +0100

    init matches array

---
+++ snack1.php
@@ -0,0 +1,66 @@
+<?php
+    $matches = [
+        [
+            "Squadra 1" => "Toronto Raptors",
+            "Squadra 2" => "Philadelphia 76ers",
+            "Punteggio 1" => 110,
+            "Punteggio 2" => 103
+        ],
+        [
+            "Squadra 1" => "Cleveland Cavaliers",
+            "Squadra 2" => "Oklahoma City Thunder",
+            "Punteggio 1" => 101,
+            "Punteggio 2" => 117
+        ],
+        [
+            "Squadra 1" => "New York Knicks",
+            "Squadra 2" => "Minnesota Timberwolves",
+            "Punteggio 1" => 103,
+            "Punteggio 2" => 99
+        ],
+        [
+            "Squadra 1" => "Orlando Magic",
+            "Squadra 2" => "Detroit Pistons",
+            "Punteggio 1" => 105,
+            "Punteggio 2" => 96
+        ],
+        [
+            "Squadra 1" => "Atlanta Hawks",
+            "Squadra 2" => "Denver Nuggets",
+            "Punteggio 1" => 123,
+            "Punteggio 2" => 115
+        ],
+        [
+            "Squadra 1" => "Los Angeles Clippers",
+            "Squadra 2" => "Brooklyn Nets",
+            "Punteggio 1" => 108,
+            "Punteggio 2" => 112
+        ],
+        [
+            "Squadra 1" => "Milwaukee Bucks",
+            "Squadra 2" => "Sacramento Kings",
+            "Punteggio 1" => 128,
+            "Punteggio 2" => 115
+        ]
+    ]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>PhP Snack 1</title>
+</head>
+<body>
+
+    <!-- MAIN -->
+    <main>
+        <section>
+
+        </section>
+    </main>
+
+</body>
+</html>
